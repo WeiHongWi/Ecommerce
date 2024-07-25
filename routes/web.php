be601366e5a1f1a15f646f88fc1ab1ae9a122ec3
Willy <arty963552@gmail.com>
2024-07-25 22:51:41 +0800

    Customerize the login page by package admin template.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Backend\AdminController;
-use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -33,6 +32,4 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 
-Route::get('admin/dashboard',[AdminController::class,'dashboard'])->middleware(['auth','role:admin'])->name('admin.dashboard');
-
-Route::get('vendor/dashboard',[VendorController::class,'dashboard'])->middleware(['auth','role:vendor'])->name('vendor.dashboard');
+Route::get('admin/login',[AdminController::class,'login'])->name('admin.login');
