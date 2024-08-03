43384585855ab7e210c2491c3ea2fa65a455897c
Willy <arty963552@gmail.com>
2024-08-03 20:02:21 +0800

    Toggl button

--- app/DataTables/CategoryDataTable.php
+++ app/DataTables/CategoryDataTable.php
@@ -33,10 +33,17 @@ class CategoryDataTable extends DataTable
                 return '<i style="font-size:45px" class="'.$query->icon.'"></i>';
             })
             ->addColumn('status',function($query){
-                $button = '<label class="custom-switch mt-2">
+                if($query->status == "1"){
+                    $button = '<label class="custom-switch mt-2">
+                        <input type="checkbox" checked name="custom-switch-checkbox" class="custom-switch-input">
+                        <span class="custom-switch-indicator"></span>
+                      </label>';
+                }else{
+                    $button = '<label class="custom-switch mt-2">
                         <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input">
                         <span class="custom-switch-indicator"></span>
                       </label>';
+                }
                 return $button;
             })
             ->rawColumns(['icon','action','status'])
