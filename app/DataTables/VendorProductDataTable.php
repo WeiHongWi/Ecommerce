e49c4ef46087d04d3a1a95a772a77bbe3098c3ff
Willy <arty963552@gmail.com>
2024-08-20 19:24:27 +0800

    Isolate the product with different vendor.

--- app/DataTables/VendorProductDataTable.php
+++ app/DataTables/VendorProductDataTable.php
@@ -89,7 +89,7 @@ class VendorProductDataTable extends DataTable
      */
     public function query(Product $model): QueryBuilder
     {
-        return $model->where('vendor_id',Auth::user()->id)->newQuery();
+        return $model->where('vendor_id',Auth::user()->vendor->id)->newQuery();
     }
 
     /**
